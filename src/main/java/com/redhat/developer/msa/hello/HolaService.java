098167d2018ac71046294681fe4a0ad706006a8e
Rafael Benevides <rafabene@gmail.com>
2016-03-09 11:54:59 -0500

    Add License Headers

--- src/main/java/com/redhat/developer/msa/hello/HolaService.java
+++ src/main/java/com/redhat/developer/msa/hello/HolaService.java
@@ -1,6 +1,6 @@
-/*
+/**
  * JBoss, Home of Professional Open Source
- * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * Copyright 2016, Red Hat, Inc. and/or its affiliates, and individual
  * contributors by the @authors tag. See the copyright.txt in the
  * distribution for a full listing of individual contributors.
  *
@@ -28,7 +28,7 @@ public class HolaService {
     @GET
     @Path("/")
     public String sayHello() {
-        String hostname = null; 
+        String hostname = null;
         try {
             hostname = InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException e) {
